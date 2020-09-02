24276dd73e64d8bb1d2457d21328baa744abcd12
durantdurant95 <apduran95@gmail.com>
2020-09-02 18:03:42 -0400

    Updates

---
+++ precache-manifest.f54d759a8a1b8ae74ebb1592b1e5b943.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb9ecb8d4adfd212eb9899bbc636bf7b",
+    "url": "/happy_havana/index.html"
+  },
+  {
+    "revision": "d6aeef03e043bcb46af5",
+    "url": "/happy_havana/static/css/main.b8fd9051.chunk.css"
+  },
+  {
+    "revision": "e90359a79b8a1274da40",
+    "url": "/happy_havana/static/js/2.2462078e.chunk.js"
+  },
+  {
+    "revision": "8f409f5a3af18e18de7597d9bfa7e6f3",
+    "url": "/happy_havana/static/js/2.2462078e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6aeef03e043bcb46af5",
+    "url": "/happy_havana/static/js/main.d9b3266e.chunk.js"
+  },
+  {
+    "revision": "c68d26ea464ef18b64d7",
+    "url": "/happy_havana/static/js/runtime-main.1d34f999.js"
+  },
+  {
+    "revision": "324a513cb9c683dc30777bdbccf59995",
+    "url": "/happy_havana/static/media/JosefinSans-VariableFont_wght.324a513c.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/happy_havana/precache-manifest.42fbcaf543c610bed00a3c916ff7eb02.js"
+  "/happy_havana/precache-manifest.f54d759a8a1b8ae74ebb1592b1e5b943.js"
 );
 
 self.addEventListener('message', (event) => {

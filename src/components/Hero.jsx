c748d411a73494681750f81c1793be5a1c351450
durantdurant95 <apduran95@gmail.com>
2020-09-02 14:22:06 -0400

    finally

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
       <h1 className="my-5 display-3">Happy Havana</h1>
       <p>Catalogamos los mejores bares, cafeterías y similares en la ciudad</p>
       <Button className="mr-5">Descarga</Button>
-      <NavLink to="/happy_havana/formulario">
+      <NavLink to="/formulario">
         <Button>Anúnciate</Button>
       </NavLink>
     </Container>

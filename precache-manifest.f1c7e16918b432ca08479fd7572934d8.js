472629ea42b5163ec8e62daba8cabbb571365f0d
durantdurant95 <apduran95@gmail.com>
2020-09-29 21:10:49 -0400

    Updates

---
+++ precache-manifest.f1c7e16918b432ca08479fd7572934d8.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f7869987a2abb93ac0ba973d436885d",
+    "url": "/happy_havana/index.html"
+  },
+  {
+    "revision": "533f3247b6d7e67fb647",
+    "url": "/happy_havana/static/css/main.9abb9ed2.chunk.css"
+  },
+  {
+    "revision": "3ee229eaa070b9fcd5c5",
+    "url": "/happy_havana/static/js/2.7622b2fc.chunk.js"
+  },
+  {
+    "revision": "8f409f5a3af18e18de7597d9bfa7e6f3",
+    "url": "/happy_havana/static/js/2.7622b2fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "533f3247b6d7e67fb647",
+    "url": "/happy_havana/static/js/main.fc3fef05.chunk.js"
+  },
+  {
+    "revision": "c68d26ea464ef18b64d7",
+    "url": "/happy_havana/static/js/runtime-main.1d34f999.js"
+  },
+  {
+    "revision": "324a513cb9c683dc30777bdbccf59995",
+    "url": "/happy_havana/static/media/JosefinSans-VariableFont_wght.324a513c.ttf"
+  },
+  {
+    "revision": "6322ee6a4b5daf0deb042e1cfd088e36",
+    "url": "/happy_havana/static/media/Jost-VariableFont_ital_wght.6322ee6a.ttf"
+  },
+  {
+    "revision": "aaa5880c7a5f7e479e31a4412452d8a9",
+    "url": "/happy_havana/static/media/Satisfy-Regular.aaa5880c.ttf"
+  },
+  {
+    "revision": "8bda5028d5971693fc9d657cfad69852",
+    "url": "/happy_havana/static/media/background_form.8bda5028.jpg"
+  },
+  {
+    "revision": "24219b64c442e0618d3c912302e6688f",
+    "url": "/happy_havana/static/media/error.24219b64.svg"
+  },
+  {
+    "revision": "aa53298d50187493dae192f217b4effd",
+    "url": "/happy_havana/static/media/icono_hh.aa53298d.svg"
+  },
+  {
+    "revision": "b1b5e3e9923665fca5a3ebc8fcfb30c3",
+    "url": "/happy_havana/static/media/ok.b1b5e3e9.svg"
+  },
+  {
+    "revision": "70ba0a66e23c18ca518adce19b8dbfe5",
+    "url": "/happy_havana/static/media/wallpaper_hero.70ba0a66.jpg"
+  },
+  {
+    "revision": "c57ae8bf7185efe16e8ab2eb143dbfb3",
+    "url": "/happy_havana/static/media/wallpaper_hero_2.c57ae8bf.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/happy_havana/precache-manifest.2288757d56b3ede09af35890acc4a293.js"
+  "/happy_havana/precache-manifest.f1c7e16918b432ca08479fd7572934d8.js"
 );
 
 self.addEventListener('message', (event) => {
